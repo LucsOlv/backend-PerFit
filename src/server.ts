c44279ae8ef0f81035fd0eabb3799a915bc54cd1
LucsOlv <lucs.oliv9@gmail.com>
2023-04-06 13:31:49 -0300

    server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import { App } from '@/app';
 import { AuthRoute } from '@routes/auth.route';
 import { UserRoute } from '@routes/users.route';
-import { ValidateEnv } from '@utils/validateEnv';
 
-ValidateEnv();
+//ValidateEnv();
 
 const app = new App([new UserRoute(), new AuthRoute()]);
 
